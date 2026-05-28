e15f5583fcb4210e03061c5c907440d09d06b64a
theagoicea <thea.goicea.tg@gmail.com>
2026-05-28 20:20:24 +0200

    Delete deconnexion.php

--- deconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();    // Vide toutes les variables de session
-session_destroy();  // Détruit la session
-
-// Redirige vers la page d'authentification
-header("Location: Authentification.php");
-exit();
-?>
