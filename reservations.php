7627fd3f54dd1dc8e1e24dc1d75cb08357df693f
louisegodlewski <louise.godlewski@edu.ece.fr>
2026-05-30 16:08:44 +0200

    Update reservations.php

--- reservations.php
+++ reservations.php
@@ -1,9 +1,4 @@
 <?php
-// 1. Activation des erreurs pour le développement (à enlever en production)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
 require_once 'configuration.php'; 
 
